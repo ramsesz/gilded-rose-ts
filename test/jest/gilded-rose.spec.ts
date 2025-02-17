a684f13e9dde5e28d012d6d0d99f3f6066b32d14
Ramses Zeulevoet <ramses@ramsesz.be>
2025-02-17 21:23:34 +0100

    test: fix test the easy way and add test for 100% coverage

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,21 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
+  it('should initiate gilded rose', () => {
+    const gildedRose = new GildedRose();
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items.length).toBe(0);
+  })
+
+  it('should update quality for foo item', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
+    const items = gildedRose.updateQuality();
+
+    const item = items[0];
+    expect(item.name).toBe('foo');
+    expect(item.quality).toBe(0);
+    expect(item.sellIn).toBe(-1);
   });
 });
