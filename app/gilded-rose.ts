c240c572e67399ec41165547e0d37bb2e07417a0
Ramses Zeulevoet <ramses@ramsesz.be>
2025-02-19 11:38:25 +0100

    chore: remove obsolete leftover updateQuality code

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -100,53 +100,6 @@ export class GildedRose {
 
       this.updateGeneralItem(item);
       return;
-
-      if (
-        item.name != ITEM_NAMES.AGED_BRIE &&
-        item.name != ITEM_NAMES.BACKSTAGE_PASSES
-      ) {
-        if (item.quality > 0) {
-          if (item.name != ITEM_NAMES.SULFURAS) {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == ITEM_NAMES.BACKSTAGE_PASSES) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name != ITEM_NAMES.SULFURAS) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != ITEM_NAMES.AGED_BRIE) {
-          if (item.name != ITEM_NAMES.BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
-              if (item.name != ITEM_NAMES.SULFURAS) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
     });
 
     return this.items;
