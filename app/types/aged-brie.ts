2ad5f12b02229e6edcca41385d2b47ac1dca426c
Ramses Zeulevoet <ramses@ramsesz.be>
2025-02-20 21:20:39 +0100

    refactor: inherit from GeneralItem to consolidate shared logic

--- app/types/aged-brie.ts
+++ app/types/aged-brie.ts
@@ -1,12 +1,7 @@
 import { Item } from "@/gilded-rose";
+import { GeneralItem } from "@/types/general-item";
 
-export class AgedBrie {
-  item: Item;
-
-  constructor(item: Item) {
-    this.item = item;
-  }
-
+export class AgedBrie extends GeneralItem {
   update() {
     this.item.sellIn -= 1;
     this.item.quality += 1;
--- app/types/sulfuras.ts
+++ app/types/sulfuras.ts
@@ -1,13 +1,9 @@
 import { Item } from "@/gilded-rose";
+import { GeneralItem } from "@/types/general-item";
 
-export class Sulfuras {
-  item: Item;
-
-  constructor(item: Item) {
-    this.item = item;
-  }
-
+export class Sulfuras extends GeneralItem {
   update() {
+    // do nothing
     return this.item;
   }
 }
