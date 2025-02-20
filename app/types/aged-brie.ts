ed9b4727b18a77c9f7ecd93ffc18b199e548e8cb
Ramses Zeulevoet <ramses@ramsesz.be>
2025-02-20 21:28:59 +0100

    refactor: move quality limits logic to helper function

--- app/types/aged-brie.ts
+++ app/types/aged-brie.ts
@@ -9,9 +9,8 @@ export class AgedBrie extends GeneralItem {
     if (this.item.sellIn < 0) {
       this.item.quality += 1;
     }
-    if (this.item.quality > 50) {
-      this.item.quality = 50;
-    }
+    
+    this.ensureQuality();
 
     return this.item;
   }
